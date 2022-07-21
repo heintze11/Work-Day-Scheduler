8076225c77ac1f758f62a9e8e9a7f0553b002fdc
Levi H <levi.heintzelman@gmail.com>
2022-07-21 14:23:40 -0600

    add textarea and start on JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,11 +5,30 @@
     //to local storage and on page
 
 // Add time and date at top of page using moment.js
-    function time (){
-        var today = moment();
-        $("#currentDay").text(today.format("dddd, MMM Do"));
-    };
-    time();
+let timeBlocks = $(".textarea");
+let calTime = $("")
+
+function time (){
+    let today = moment();
+    $("#currentDay").text(today.format("dddd, MMM Do"));
+};
+time();
     // setInterval(function(){
     //     time();
     // }, 1000);
+
+
+//compare current time with timeblock time
+
+//create for loop to go through each timeblock
+//crete if statement to add color to text area
+//set interval to check the time every x mins to update colors
+
+
+function checkTime (){
+    let currentTime = moment().format("H");
+
+    if(currentTime < #id){
+        $
+    }
+}
