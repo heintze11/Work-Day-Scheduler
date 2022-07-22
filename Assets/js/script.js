f9bf030db6ea08730c89ac89637d1cf3f097c4b1
Levi H <levi.heintzelman@gmail.com>
2022-07-22 11:45:20 -0600

    Added checktime function and setinterval

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,33 +2,48 @@
 // Add 1 hour time blocks from 9-5pm
 // color code time blocks based on current time
 // add functionality to type in hour blocks and save text
-    //to local storage and on page
+//to local storage and on page
+
+
+let timeBlocks = $("textarea");
+console.log(timeBlocks);
 
 // Add time and date at top of page using moment.js
-let timeBlocks = $(".textarea");
-let calTime = $("")
-
-function time (){
+function time() {
     let today = moment();
     $("#currentDay").text(today.format("dddd, MMM Do"));
 };
 time();
-    // setInterval(function(){
-    //     time();
-    // }, 1000);
+// setInterval(function(){
+//     time();
+// }, 1000);
 
 
 //compare current time with timeblock time
-
 //create for loop to go through each timeblock
-//crete if statement to add color to text area
-//set interval to check the time every x mins to update colors
-
-
-function checkTime (){
+//create if statement to add color to text area
+function checkTime() {
     let currentTime = moment().format("H");
-
-    if(currentTime < #id){
-        $
+    //remove old classes
+    $("textarea").removeClass("present past future");
+    
+    for (let i = 0; i < timeBlocks.length; i++) {
+        let calTime = timeBlocks[i].id;      
+               
+        if(currentTime < calTime){
+            $(timeBlocks[i]).addClass("future");
+        } else if(currentTime > calTime){
+            $(timeBlocks[i]).addClass("past");
+        } else{
+            $(timeBlocks[i]).addClass("present");
+        }
+              
     }
 }
+checkTime()
+
+//set interval to check the time every x mins to update colors
+setInterval(function(){
+    time();
+    checkTime();
+}, 60000);
